cebbfa51ec9fc647234f339f29bacf7f5e7a4fb8
omarba136 <omarbalad2002@.com>
2025-06-13 16:36:21 +0300

    create notes view model

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,18 @@
+class NotesModel {
+  final String titel;
+  final String subtitel;
+  final String date;
+  final int color;
+
+
+
+NotesModel({
+  required this.titel,
+  required this.subtitel,
+  required this.date,
+  required this.color,
+});
+
+
+
+}
