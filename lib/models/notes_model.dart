b30a7e9751b103990b70b9c4cb49978e71aafb72
omarba136 <omarbalad2002@.com>
2025-06-15 00:15:20 +0300

    craete tybe adabter for note model

--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,13 +1,23 @@
-class NotesModel {
-  final String titel;
+import 'package:hive/hive.dart';
+
+
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotesModel extends HiveObject{
+  @HiveField(0)
+ final String title;
+   @HiveField(1)
   final String subtitel;
+   @HiveField(2)
   final String date;
+   @HiveField(3)
   final int color;
 
 
 
 NotesModel({
-  required this.titel,
+  required this.title,
   required this.subtitel,
   required this.date,
   required this.color,
